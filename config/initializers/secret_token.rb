f83265926adbc51d8f4f40c5b1a37a5e3d30bcd9
Elyse Dougherty <elysedougherty@gmail.com>
2011-10-20 21:36:27 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Schroeder::Application.config.secret_token = '214dd9d61bd12cd9fbfafdec3b0d6574448c0ecbf1d6c27414b99f662e3e0050cb99348b02885ec25878b0aaec49bf9ee7f7fccd6ee38a5f6c9feb03eb7c048e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Schroeder::Application.config.session_store :cookie_store, :key => '_schroeder_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Schroeder::Application.config.session_store :active_record_store
